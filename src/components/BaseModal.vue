62dc466dc38e0d2df8df9730f91994d100df6cd9
Diego Melgar <diego.melgar.sv@gmail.com>
2023-05-31 16:54:13 -0600

    ✨lesson 4 - add modal with teleport and transitions

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,68 @@
+<template>
+    <Teleport to="body">
+        <Transition name="modal-outer">
+            <div v-show="modalActive" @click="closeModal" class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
+                <Transition name="modal-inner">
+                    <div @click.stop="" v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-screen-md">
+                        <slot />
+                        <button @click="$emit('close-modal')" class="text-white mt-8 bg-weather-primary py-2 px-6">Close</button>
+                    </div>
+                </Transition>
+            </div>
+        </Transition>
+    </Teleport>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    modalActive: {
+        type: Boolean,
+        default: false
+    },
+    persistent: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const emits = defineEmits(["close-modal"])
+
+const closeModal = () => {
+    if (!props.persistent) {
+        emits('close-modal');
+    }
+}
+</script>
+
+
+<style scoped>
+
+/* Modal Outer transitions */
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+    transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-outer-enter-from,
+.modal-outer-leave-to {
+    opacity: 0;
+}
+
+/* Modal Inner transitions */
+.modal-inner-enter-active {
+    transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+.modal-inner-leave-active {
+    transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-inner-enter-from {
+    opacity: 0;
+    transform: scale(0.8);
+}
+
+.modal-inner-leave-to {
+    transform: scale(0.8);
+}
+</style>
