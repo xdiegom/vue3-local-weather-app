0b11a5722a1784d87f41ffb5e4e2c2c8fdbbb4c2
Diego Melgar <diego.melgar.sv@gmail.com>
2023-06-14 14:38:52 -0600

    ✨ lesson 14 - add routes transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="flex flex-col min-h-screen font-roboto bg-weather-primary">
     <SiteNavigation />
-    <RouterView />
+    <RouterView v-slot="{ Component, route }">
+      <Transition name="page" mode="out-in">
+        <div :key="route.name">  
+          <component :is="Component" />
+        </div>
+      </Transition>
+    </RouterView>
   </div>
 </template>
 
@@ -12,4 +18,21 @@ import { RouterView } from 'vue-router';
 </script>
 
 
-<style scoped></style>
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: 300ms ease all;
+}
+
+/** 
+  By default, if "page-enter-to" and "page-leave-from" are not present, when the transition
+  state is "active", if opacity or other css attribute is not defined (as far as I understood)
+  the opacity is set to 1. That's why it is not use here
+
+*/
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
